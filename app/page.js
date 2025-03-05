8c5ed861bd231b4776891a4b35ea772d6a9af233
du050 <Dudacintraxwork@gmail.com>
2025-03-05 09:23:12 -0700

    name addition

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold mb-8 text-gray-800">CPRG 306: Web Development 2 - Assignments</h1>
+      <p>By Maria Xavier</p>
       <Link href="/week-2">
         <p className="text-lg text-blue-500 hover:text-blue-700">Go to Week 2 Assignments</p>
       </Link>
